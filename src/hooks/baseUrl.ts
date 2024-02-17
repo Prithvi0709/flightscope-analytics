b1908e87156a168dff15f311619cfa3067d08feb
Prithviraj Kalaiselvan <prithviraj792000@gmail.com>
2024-02-17 17:00:21 -0500

    Project Recording Added

--- src/hooks/baseUrl.ts
+++ src/hooks/baseUrl.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance with a base URL
 const axiosInstance = axios.create({
-  baseURL: "https://bbe3-34-91-1-38.ngrok-free.app",
+  baseURL: "https://0ff9-35-231-244-44.ngrok-free.app",
 });
 
 export default axiosInstance;
